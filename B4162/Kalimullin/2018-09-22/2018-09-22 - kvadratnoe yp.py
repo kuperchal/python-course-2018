aa58f2452d42fb487f4f92c0806f2aa69855ef6b
192117 <43795507+192117@users.noreply.github.com>
2018-10-06 22:07:58 +0300

    homework fix

--- B4162/Kalimullin/2018-09-22/2018-09-22 - kvadratnoe yp.py
+++ B4162/Kalimullin/2018-09-22/2018-09-22 - kvadratnoe yp.py
@@ -1,11 +1,10 @@
-import math
 a=int(input('Введите 1 коэффициент = '))
 b=int(input('Введите 2 коэффициент = '))
 c=int(input('Введите 3 коэффициент = '))
 D=b**2-4*a*c
 if D>0:
-    print('x1 =',(-b-(math.sqrt(D)))/(2*a))
-    print('x2 =',(-b+(math.sqrt(D)))/(2*a))
+    print('x1 =',(-b-((D)**(1/2)))/(2*a))
+    print('x2 =',(-b+((D)**(1/2)))/(2*a))
 elif D==0:
     print('x =',(-b/(2*a)))
 else: print('Корней нет')
