3d9f5935a5af7320498ac30f2a10418987e88588
Surfin1996 <43863900+Surfin1996@users.noreply.github.com>
2018-10-20 00:41:42 +0300

    ДЗ №2
    Второе домашнее задание с использованием парадигм ООП

---
+++ B4162/Kozlov/B4162-Kozlov-homework-2018-10-06.py
@@ -0,0 +1,147 @@
+class Figure:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def square(self):
+        return self.x * self.y
+
+
+class Rectangle(Figure):
+    def __s(self):
+        return self.x * self.y
+
+    def square(self):
+        return self.__s()
+
+
+class Triangle(Figure):
+    def __s(self):
+        return 0.5 * self.x * self.y
+
+    def square(self):
+        return self.__s()
+
+
+rect = Rectangle(1, 2)
+print(rect.square())
+
+trgl = Triangle(23, 3)
+print(trgl.square())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
