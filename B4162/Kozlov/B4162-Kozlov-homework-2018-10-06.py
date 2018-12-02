c0c10c803d57af90c65ea1147847ec4592ad6631
Surfin1996 <43863900+Surfin1996@users.noreply.github.com>
2018-12-02 15:07:03 +0300

    Fixed
    Homework was fixed

--- B4162/Kozlov/B4162-Kozlov-homework-2018-10-06.py
+++ B4162/Kozlov/B4162-Kozlov-homework-2018-10-06.py
@@ -1,10 +1,11 @@
 class Figure:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
+        self.z = z
 
-    def square(self):
-        return self.x * self.y
+    #def square(self):
+        #return self.x * self.y
 
 
 class Rectangle(Figure):
@@ -22,12 +23,23 @@ class Triangle(Figure):
     def square(self):
         return self.__s()
 
+class Parallelepiped(Figure):
+    def __s(self):
+        return 2 * (self.x * self.y + self.y * self.z + self.x * self.z)
+    
+    def square(self):
+        return self.__s()
 
 rect = Rectangle(1, 2)
 print(rect.square())
 
 trgl = Triangle(23, 3)
 print(trgl.square())
+
+prlpd = Parallelepiped(1,2,3) #Добавил ещё один параметр для более универсального нахождения площади фигуры, 
+                              #в частности 3Д фигуры (параллелепипед). Парадигмы ООП (инкапсуляции, полиморфизма и наследования)
+                              #по заданию использованы.
+print(prlpd.square())
 
 
 
