5ac1fe8fc1094d23524ece90e1c557ef413e74db
Surfin1996 <43863900+Surfin1996@users.noreply.github.com>
2018-12-10 23:41:08 +0300

    Fixed_2

--- B4162/Kozlov/B4162-Kozlov-homework-2018-10-06.py
+++ B4162/Kozlov/B4162-Kozlov-homework-2018-10-06.py
@@ -1,159 +1,54 @@
 class Figure:
-    def __init__(self, x=0, y=0, z=0):
-        self.x = x
-        self.y = y
-        self.z = z
+    def get_s(self): 
+        pass
+    def get_type(self):
+        pass
+#    def __init__(self, x=0, y=0, z=0):
+#        self.x = x
+#        self.y = y
+#        self.z = z
 
     #def square(self):
         #return self.x * self.y
 
-
 class Rectangle(Figure):
-    def __s(self):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+#    def __s(self):
+    def get_s(self):
         return self.x * self.y
+    def get_type(self):
+        return "Прямоугольник"
 
-    def square(self):
-        return self.__s()
-
-
-class Triangle(Figure):
-    def __s(self):
+class Triangle_90(Figure):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    def get_s(self):
         return 0.5 * self.x * self.y
-
-    def square(self):
-        return self.__s()
+    def get_type(self):
+        return "Прямоугольный_треугольник"
 
 class Parallelepiped(Figure):
-    def __s(self):
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+    def get_s(self):
         return 2 * (self.x * self.y + self.y * self.z + self.x * self.z)
+    def get_type(self):
+        return "Параллепипед"
     
-    def square(self):
-        return self.__s()
-
 rect = Rectangle(1, 2)
-print(rect.square())
+print(rect.get_s())
+print (rect.get_type())
 
-trgl = Triangle(23, 3)
-print(trgl.square())
+trgl = Triangle_90(23, 3)
+print(trgl.get_s())
+print (trgl.get_type())
 
 prlpd = Parallelepiped(1,2,3) #Добавил ещё один параметр для более универсального нахождения площади фигуры, 
-                              #в частности 3Д фигуры (параллелепипед). Парадигмы ООП (инкапсуляции, полиморфизма и наследования)
-                              #по заданию использованы.
-print(prlpd.square())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(prlpd.get_s())
+print (prlpd.get_type())        #в частности 3Д фигуры (параллелепипед). Парадигмы ООП (инкапсуляции, полиморфизма и наследования)
+                                            #по заданию использованы.
