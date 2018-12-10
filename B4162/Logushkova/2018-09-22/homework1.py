a053d14faedc1a72d50ad0ac098941e7e9d43139
wecsog <44035011+wecsog@users.noreply.github.com>
2018-12-10 21:54:49 +0300

    Update homework1.py

--- B4162/Logushkova/2018-09-22/homework1.py
+++ B4162/Logushkova/2018-09-22/homework1.py
@@ -1,18 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 10 23:38:00 2018
-
-@author: User
-"""
-
-import math
 a = float (input("vvedite a:"))
 b = float (input("vvedite b:"))
 c = float (input("vvedite c:"))
 d = b**2 - 4 *a*c
 if d > 0:
-    x1 = (-b + math.sqrt(d)) / (2* a)
-    x2 = (-b - math.sqrt(d)) / (2* a)    
+    x1 = (-b + d**0.5) / (2* a)
+    x2 = (-b - d**0.5) / (2* a)    
     print ("Answer: x1=", x1, "x2=", x2 )
 elif d == 0:
     x1 = -b / (2*a)
