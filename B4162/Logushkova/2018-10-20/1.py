a684f0c3edfa1aed1d04f506dfbf96b44b0f086b
wecsog <44035011+wecsog@users.noreply.github.com>
2018-12-11 22:35:32 +0300

    homework fix

--- B4162/Logushkova/2018-10-20/1.py
+++ B4162/Logushkova/2018-10-20/1.py
@@ -1,32 +1,40 @@
-class strk:
+class mass:
  def __init__(self, m):
    self.m = m
-   #выводит строку
+   #выводит массив
  def show(self):
     print(self.m)
-   #сортировка от меньшего к большему
+   #сортировка от меньшего к большему для строк массива
  def sortt(self):
-      self.m.sort() 
-  #выводит уникальные элементы
- def uniq(self):
-     a = set(self.m)
-     print(a)
+     for i in range(len(self.m)):
+      self.m[i].sort() 
+  #выводит уникальные элементы строк массива
+ def uniq(self):   
+   z = []                                #объединение строк матрицы (двумерного массива) в одну строку (одномерный массив)
+   for i in range(len(self.m)):
+     for j in range(len(self.m[i])):
+       newelement = self.m[i][j]
+       z.append(newelement)
+   a = set(z) 
+   print(a)
   #выводит элемент с максимальным значением
  def maximal (self):
-     self.mx = self.m[0]
+     self.mx = self.m[0][0]
      for i in range(len(self.m)):
-       if  self.m[i] > self.mx:
-              self.mx = self.m[i]
+      for j in range(len(self.m[i])):
+       if  self.m[i][j] > self.mx:
+              self.mx = self.m[i][j]
      print(self.mx)
   #выводит элемент с минимальным значением
  def minimal (self):
-     self.ms = self.m[0]
+     self.ms = self.m[0][0]
      for i in range(len(self.m)):
-         if self.ms > self.m[i]:
-              self.ms = self.m[i]
+         for j in range(len(self.m[i])):
+          if self.ms > self.m[i][j]:
+              self.ms = self.m[i][j]
      print(self.ms)
     
-b = strk([-9, -33, -2, -3, -5, -5, -800, -9, -3, -1, -2, -11, -7, 263, -6, -4, -9])
+b = mass([[-9, -33, -2, -3, -5, -5, -800],[ -9, -3, -1, -2, -11, -7, 263, -6, -4, -9]])
 b.show()
 b.sortt()
 b.show()
