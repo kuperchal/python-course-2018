0c260e28b9d757506a102921e1454e83c99317ad
wecsog <44035011+wecsog@users.noreply.github.com>
2018-12-11 22:38:24 +0300

    homework fix

--- B4162/Logushkova/2018-12-01/матплот.py
+++ B4162/Logushkova/2018-12-01/матплот.py
@@ -29,4 +29,29 @@ plt.grid(True)
 #редактирование
 plt.subplots_adjust(left=0.1, right=0.95, bottom=0.5, top=2, wspace=0.3, hspace=0.3) 
 plt.show()
-
+#вывод отдельного графика
+a = int (input("для вывода отдельного графика введите цифру: 1 - синус, 2 - косинус, 3 - тангенс, 4 - котангенс:"))
+if a == 1:
+    plt.figure(figsize=(15, 10))
+    plt.plot(x, np.sin(x))
+    plt.title('sin(x)') 
+    plt.show()
+if a== 2:
+    plt.figure(figsize=(10, 5))
+    plt.plot(x, np.cos(x), 'g')
+    plt.grid(True) 
+    plt.title('cos(x)')
+    plt.show()
+if a== 3:
+    plt.figure(figsize=(15, 10))
+    plt.plot(x, np.tan(x), 'r')
+    plt.axis([-2*np.pi, 2*np.pi, -10, 10])
+    plt.title('tan(x)')
+    plt.show()
+if a == 4:
+    plt.figure(figsize=(15, 10))
+    plt.plot(x, np.cos(x)/np.sin(x), 'b')
+    plt.axis([-2*np.pi, 2*np.pi, -10, 10])
+    plt.title('ctg(x)')
+    plt.grid(True)
+    plt.show()
