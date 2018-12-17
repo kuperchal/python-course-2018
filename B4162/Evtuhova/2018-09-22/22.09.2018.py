787249b78c32bfc0f40e29407fa5d16c6c4d21f8
dariaroshior <45767898+dariaroshior@users.noreply.github.com>
2018-12-17 22:13:56 +0300

    homework-2018-09-22-fixx

--- B4162/Evtuhova/2018-09-22/22.09.2018.py
+++ B4162/Evtuhova/2018-09-22/22.09.2018.py
@@ -5,9 +5,8 @@ c = float(input("c = "))
 d = b**2 - 4 * a * c
 print("d = ", d)
 if d > 0:
-	import math
-	x1 = (-b + math.sqrt(d)) / (2 * a)
-	x2 = (-b - math.sqrt(d)) / (2 * a)
+	x1 = (-b-(d**(1/2)))/(2*a)
+	x2 = (-b+(d**(1/2)))/(2*a)
 	print("x1 = ", x1, "x2 = ", x2)
 elif d == 0:
 	x = -b / (2 * a)
