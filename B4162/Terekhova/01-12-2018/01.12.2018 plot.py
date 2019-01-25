7b57c67305462e9cc9473486277536f7a2967d21
AnnaTerekhova <44551657+AnnaTerekhova@users.noreply.github.com>
2019-01-25 23:40:33 +0300

    01-12-2018

---
+++ B4162/Terekhova/01-12-2018/01.12.2018 plot.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def graph():
+    x=np.arange(-6,6.01,0.01) #Массив значений аргумента для Синуса и Косинуса
+    x1=np.arange(-1.56,1.57,0.05) #Массив значений аргумента для тангенса
+    c=np.arange(0,3.15,0.05) #Массив значений аргумента для котангенса
+    plt.subplot(221) #Графическая область 1 размером 2х2
+    plt.plot(x,np.sin(x),'m') #Построение графика с определенным цветом в области Х
+    plt.title(r'$\sin(x)$') #Называем график
+    plt.xlabel(r'$x$') #Добавляем подпись для оси Х
+    plt.ylabel(r'$f(x)$') #Добавляем подпись для оси У
+    plt.annotate('local max', xy=(1.57, 1), xytext=(1.58, 1),) #Добавляем надпись local max
+    plt.annotate('local min', xy=(-1.57, -1), xytext=(-1.58, -1),) #Добавляем надпись local min
+    plt.subplot(222) #Графическая область 2 размером 2х2
+    plt.plot(x,np.cos(x),'k') #Построение графика с определенным цветом в области Х
+    plt.grid(True)
+    plt.title(r'$\cos(x)$')
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$f(x)$')
+    plt.annotate('local max', xy=(0, 1), xytext=(0.01,1),)
+    plt.annotate('local min', xy=(-3.14, -1), xytext=(-3.15, -1),)
+    plt.subplot(223)
+    plt.plot(x1,np.tan(x1),'y') #Построение графика с определенным цветом в области Х1
+    plt.title(r'$tg(x)$')
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$f(x)$')
+    plt.text (-1, -25, u"HET max u min")
+    plt.subplot(224)
+    plt.plot(c,1/np.tan(c),'c') #Построение графика с определенным цветом в области С
+    plt.title(r'$ctg(x)$')
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$f(x)$')
+    plt.text (0.5, 10, u"HET max u min")
+    plt.show()
+def pr():
+    print('Bbedute slovo')
+    sl=input()
+    if sl=='again':
+        graph()
+    else:
+        pr()
+graph()
+pr()
